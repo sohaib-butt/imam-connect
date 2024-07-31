c20086290bf7838bc9ec30d4054edd578af4f720
Sohaib Butt <sohaibbutt.work@gmail.com>
2024-07-31 17:37:57 +0500

    intro section

--- src/app/homepage/page.js
+++ src/app/homepage/page.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import GettingStarted from "@/components/GettingStarted";
+import Intro from "@/components/Intro";
 
 const page = () => {
   return (
     <div>
       <Header />
+      <Intro/>
       <GettingStarted />
       <Footer />
     </div>
---
+++ src/components/Intro.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styles from "../styles/homepage.module.scss";
+
+const Intro = () => {
+  const popularTags = [
+    "Nikah",
+    "Marriage & Family Services",
+    "Classes",
+    "Training",
+    "Counselling",
+    "Life update",
+  ];
+
+  return (
+    <section className={styles.intro}>
+      <div className={styles.inside_intro}>
+        <p className={styles.intro_find_text}>
+          FIND MUSLIM SERVICES PROVIDERS ON
+        </p>
+        <p className={styles.intro_welcome_text}>
+          Welcome to the World's First Online Hub{" "}
+          <span style={{ fontWeight: "700" }}>for Muslim Services</span>
+        </p>
+        <div style={{ width: "100%", position: "relative" }}>
+          <input
+            name="name"
+            id="name"
+            type="text"
+            className={styles.intro_search_div}
+            placeholder="Try Search: Nikah"
+          />
+          <img
+            src="assets/images/searchIcon.svg"
+            className={styles.intro_search_icon}
+          />
+        </div>
+        <div style={{ textAlign: "start", width: "100%", paddingTop: "1rem" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "start",
+              alignItems: "center",
+              textAlign: "start",
+              gap: "2rem",
+            }}
+          >
+            <p className={styles.popular_text}>Popular:</p>
+            <div className={styles.popular_list}>
+              {popularTags?.map((t, index) => (
+                <div className={styles.popular_tag}>{t}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div style={{ width: "35%", textAlign: "center" }}>
+          <button className={styles.needs_primary_button}>
+            Find your needs
+          </button>
+        </div>
+        <img
+          src="assets/images/girl-holding-books.svg"
+          style={{ marginTop: "2rem" }}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
