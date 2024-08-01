0d929442f5888a2a9259a18c51b30b375de1cde7
Sohaib Butt <sohaibbutt.work@gmail.com>
2024-08-01 23:51:48 +0500

    Madina responsiveness

--- src/components/Home/Madina.js
+++ src/components/Home/Madina.js
@@ -5,18 +5,7 @@ const Madina = () => {
   return (
     <section className={styles.madina}>
       <div className={styles.madina_gradient}></div>
-      <div
-        style={{
-          width: "50%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "start",
-          alignItems: "start",
-          gap: "1rem",
-          zIndex: "9999",
-          padding: "5% 14% 5% 0%",
-        }}
-      >
+      <div className={styles.madina_inside_div}>
         <span className={styles.madina_heading}>
           Have You Ever Dreamed of Having your Nikah Ceremony in Mecca or
           Madina?
@@ -24,7 +13,7 @@ const Madina = () => {
         <span className={styles.madina_content}>
           Now, with our exclusive Nikah Packages, you can make it into reality
         </span>
-        <div style={{ width: "50%", textAlign: "center" }}>
+        <div className={styles.nikah_packages_btn_div}>
           <button className={styles.needs_primary_button}>
             Nikah Packages
           </button>
