aa55dc02d6b84509da99bef1979ee9d65dc336fc
Sohaib Butt <sohaibbutt.work@gmail.com>
2024-08-02 00:49:37 +0500

    getting-started responsiveness

--- src/components/shared/GettingStarted.js
+++ src/components/shared/GettingStarted.js
@@ -6,14 +6,7 @@ const GettingStarted = () => {
     <section className={styles.get_started}>
       <div className={styles.get_started_card}>
         <div
-          style={{
-            width: "50%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "start",
-            alignItems: "start",
-            gap: "1rem",
-          }}
+          className={styles.get_started_card_inner}
         >
           <span className={styles.get_started_heading}>
             Lifetime Muslim Services on One Platform.
@@ -21,7 +14,7 @@ const GettingStarted = () => {
           <span className={styles.get_started_content}>
             We offer a lot of services in just 1 click and simple.
           </span>
-          <div style={{ width: "50%", textAlign: "center" }}>
+          <div className={styles.get_started_btn_div}>
             <button className={styles.needs_primary_button}>
               Let’s get started
             </button>
@@ -29,7 +22,7 @@ const GettingStarted = () => {
         </div>
         <img
           src="assets/images/happy-girl.svg"
-          className={styles.get_started_img}
+          className={`${styles.get_started_img} hidden lg:block`}
         />
       </div>
     </section>
