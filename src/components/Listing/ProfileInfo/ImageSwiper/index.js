c7206ae866063717d72f4a9e43aa94d5aca54b34
Sohaib Butt <sohaibbutt.work@gmail.com>
2024-08-13 22:03:52 +0500

    listing page imageSlider completed

--- src/components/Listing/ProfileInfo/ImageSwiper/index.js
+++ src/components/Listing/ProfileInfo/ImageSwiper/index.js
@@ -19,7 +19,7 @@ const images = [
 
 const index = () => {
   return (
-    <div className="w-full max-w-lg mx-auto">
+    <div className="w-full">
       <Swiper
         modules={[Navigation, Pagination]}
         navigation={{
@@ -40,16 +40,18 @@ const index = () => {
                 alt={`Slide ${index + 1}`}
                 width={600}
                 height={400}
-                className="object-cover rounded-[13px]"
+                className="object-cover rounded-[13px] w-full"
               />
-              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white font-poppins font-[700] text-[12px] md:text-[19px] py-2 pr-2 rounded-b-[13px] text-end">
+              <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-[#2A282F] to-[#2A282F00] text-white font-poppins font-[700] text-[12px] md:text-[19px] pt-6 pb-4 pr-4 rounded-b-[13px] text-end">
                 {index + 1}/8
               </div>
             </div>
           </SwiperSlide>
         ))}
-        <div className="swiper-button-prev bg-[#442d87] rounded-[50%] !text-white after:!text-[12px] after:!font-[700] !w-[75px] !h-[75px] absolute top-1/2 transform -translate-y-1/2 left-[-40px]"></div>
-        <div className="swiper-button-next bg-[#442d87] rounded-[50%] !text-white after:!text-[12px] after:!font-[700] !w-[75px] !h-[75px]"></div>
+        <div className="swiper-button-prev bg-[#442d87] !w-[38px] !h-[75px] flex items-center justify-center rounded-tr-[50px] rounded-br-[50px] !text-white after:!text-[12px] after:!font-[700] !left-0">
+        </div>
+        <div className="swiper-button-next bg-[#442d87] !w-[38px] !h-[75px] flex items-center justify-center rounded-tl-[50px] rounded-bl-[50px] !text-white after:!text-[12px] after:!font-[700] !right-0">
+        </div>
       </Swiper>
     </div>
   );
