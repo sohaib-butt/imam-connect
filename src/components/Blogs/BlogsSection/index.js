806a0a317823405b45e55a8e7f80e6f67414b5f3
Sohaib Butt <sohaibbutt.work@gmail.com>
2024-08-17 02:24:21 +0500

    latestTopics

--- src/components/Blogs/BlogsSection/index.js
+++ src/components/Blogs/BlogsSection/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchSection from "./SearchSection";
 import Tags from "./Tags";
 import TrendingTopic from "./TrendingTopic";
+import LatestTopics from "./LatestTopics";
 
 const index = () => {
   return (
@@ -12,6 +13,7 @@ const index = () => {
       <SearchSection />
       <Tags />
       <TrendingTopic />
+      <LatestTopics />
     </div>
   );
 };
