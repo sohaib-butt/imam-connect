ce4164668a45213b2381a8112f9b1a302164067b
Sohaib Butt <sohaibbutt.work@gmail.com>
2024-09-07 23:59:57 +0500

    user-calendar

--- src/components/UserDashboard/index.js
+++ src/components/UserDashboard/index.js
@@ -1,3 +1,4 @@
 export { default as Settings } from "./Settings";
 export { default as Favourite } from "./Favourite";
 export { default as Purchase } from "./Purchase";
+export { default as Calendar } from "./Calendar";
