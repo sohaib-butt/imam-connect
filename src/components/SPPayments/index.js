d8abbf89eb7ff040bce57386d1a73d05acac1f7b
Sohaib Butt <sohaibbutt.work@gmail.com>
2024-09-11 01:12:20 +0500

    registeration-completed

--- src/components/SPPayments/index.js
+++ src/components/SPPayments/index.js
@@ -1,2 +1,3 @@
 export { default as AddPayment } from "./AddPayment";
 export { default as CreatePassword } from "./CreatePassword";
+export { default as RegisterCompleted } from "./RegisterCompleted";
